46d58c885c49716470cd08e40bd465c478a829e4
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-02-16 15:15:30 +0530

    database

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
 
 // const todoRoutes = require('./routes/todoRoutes');
 // app.use('/todos', todoRoutes);
